e13fdc456b62114463f07b988e57095ff62c8939
Beda Vladimir <duracellp@yandex.ru>
2024-03-03 02:04:16 +0300

    Добавлена параметризация групп и контактов

--- address_book_web_tests/src/test/java/tests/CreateContactTest.java
+++ address_book_web_tests/src/test/java/tests/CreateContactTest.java
@@ -2,14 +2,42 @@ package tests;
 import model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateContactTest extends TestBase{
+    public static List<ContactData> contactProvider() {
+        var result = new ArrayList<ContactData>(List.of(
+                new ContactData(),
+                new ContactData().withFirstName("Test"),
+                new ContactData().withLastName("Test"),
+                new ContactData().withAddress("Test address"),
+                new ContactData().withNumber("Test number"),
+                new ContactData().withEmail("Test email")));
+        for (int i = 0; i < 3; i++) {
+            result.add(new ContactData(randomString(i * 10), randomString(i * 10), randomString(i * 10),
+                    randomString(i * 10), randomString(i * 10)));
+        }
+        return result;
+    }
+
     @Test
     public void createContactTest() {
         int countContactBef = app.contact().getCountContact();
         app.contact().clickContactLink();
         app.contact().createContact(new ContactData().withFirstName("Ivan").withLastName("Ivanov")
                 .withAddress("Lenin Street"));
+        int countContactAft = app.contact().getCountContact();
+        Assertions.assertEquals(countContactBef + 1, countContactAft);
+    }
+    @ParameterizedTest
+    @MethodSource("contactProvider")
+    public void createMultipleContacts(ContactData contact) {
+        int countContactBef = app.contact().getCountContact();
+        app.contact().clickContactLink();
+        app.contact().createContact(contact);
         int countContactAft = app.contact().getCountContact();
         Assertions.assertEquals(countContactBef + 1, countContactAft);
     }
--- address_book_web_tests/src/test/java/tests/CreateGroupTest.java
+++ address_book_web_tests/src/test/java/tests/CreateGroupTest.java
@@ -2,12 +2,36 @@ package tests;
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class CreateGroupTest extends TestBase {
+    public static List<GroupData> groupProvider() {
+        var result = new ArrayList<GroupData>(List.of(
+                new GroupData("test name", "", ""),
+                new GroupData(),
+                new GroupData().withName("Name test").withHeader("Test").withFooter("Test")));
+        for (int i = 0; i < 5; i++) {
+            result.add(new GroupData(randomString(i * 10), randomString(i * 10), randomString(i * 10)));
+        }
+        return result;
+    }
+
     @Test
     public void createGroupTest() {
         int groupCountBef = app.groups().getGroupCount();
         app.groups().createGroup(new GroupData("Test name", "Test header", "Test footer"));
+        int groupCountAft = app.groups().getGroupCount();
+        Assertions.assertEquals(groupCountBef + 1, groupCountAft);
+    }
+    @ParameterizedTest
+    @MethodSource("groupProvider")
+    public void createMultipleGroups(GroupData group) {
+        int groupCountBef = app.groups().getGroupCount();
+        app.groups().createGroup(group);
         int groupCountAft = app.groups().getGroupCount();
         Assertions.assertEquals(groupCountBef + 1, groupCountAft);
     }
